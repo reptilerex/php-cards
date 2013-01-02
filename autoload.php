101e0fd6c707a59166f94f22d29a89361c3bd5d8
mstraczkowski <mstraczkowski@mstraczkowski>
2013-01-02 18:09:04 +0100

    Made autoloader and simple developemnt test enviroment

---
+++ autoload.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Function phpCardsAutoloader();
+ * 
+ * Function is simple autoloader which is
+ * registered by spl_autoload_register, it
+ * is very simple to make a work easier
+ * it is earsing PHPCards from classname
+ * and replace "\" to directory separator
+ * PHPCards are PSR-0 friendly so it is
+ * very simple to implement
+ * 
+ * @param	string	$sClass	Class to load
+ * @return	void
+ */
+function phpCardsAutoloader($sClass)
+{
+	$sBase = dirname(__FILE__).DIRECTORY_SEPARATOR;
+	$aSearch = array('PHPCards', '\\');
+	$aReplace = array('', DIRECTORY_SEPARATOR);
+	
+	$sPath = str_replace($aSearch, $aReplace, $sClass).'.php';
+	include_once($sBase.$sPath);
+}//end of phpCardsAutoloader() function
+
+spl_autoload_register('phpCardsAutoloader');
---
+++ testingCards.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Using namespaces
+ */
+use PHPCards\Cards\Ace;
+use PHPCards\Cards\Ten;
+use PHPCards\Cards\Nine;
+use PHPCards\Cards\King;
+use PHPCards\Cards\Queen;
+use PHPCards\Cards\Jack;
+use PHPCards\Suits\Heart;
+use PHPCards\Suits\Diamond;
+use PHPCards\Suits\Spade;
+use PHPCards\Suits\Club;
+use PHPCards\Player;
+use PHPCards\Deck;
+
+// Autoloading
+$sPath = dirname(__FILE__).DIRECTORY_SEPARATOR;
+include_once($sPath.'autoload.php');
+
+// Aces
+$oAceHeart = new Ace();		$oAceHeart->setSuit(new Heart());
+$oAceDiamond = new Ace();	$oAceDiamond->setSuit(new Diamond());
+$oAceSpade = new Ace();		$oAceSpade->setSuit(new Spade());
+$oAceClub = new Ace();		$oAceClub->setSuit(new Club());
+
+// Tens
+$oTenHeart = new Ten();		$oTenHeart->setSuit(new Heart());
+$oTenDiamond = new Ten();	$oTenDiamond->setSuit(new Diamond());
+$oTenSpade = new Ten();		$oTenSpade->setSuit(new Spade());
+$oTenClub = new Ten();		$oTenClub->setSuit(new Club());
+
+// Nines
+$oNineHeart = new Nine();		$oNineHeart->setSuit(new Heart());
+$oNineDiamond = new Nine();	$oNineDiamond->setSuit(new Diamond());
+$oNineSpade = new Nine();		$oNineSpade->setSuit(new Spade());
+$oNineClub = new Nine();		$oNineClub->setSuit(new Club());
+
+// Jacks
+$oJackHeart = new Jack();		$oJackHeart->setSuit(new Heart());
+$oJackDiamond = new Jack();	$oJackDiamond->setSuit(new Diamond());
+$oJackSpade = new Jack();		$oJackSpade->setSuit(new Spade());
+$oJackClub = new Jack();		$oJackClub->setSuit(new Club());
+
+// Queens
+$oQueenHeart = new Queen();	$oQueenHeart->setSuit(new Heart());
+$oQueenDiamond = new Queen();	$oQueenDiamond->setSuit(new Diamond());
+$oQueenSpade = new Queen();	$oQueenSpade->setSuit(new Spade());
+$oQueenClub = new Queen();		$oQueenClub->setSuit(new Club());
+
+// Kings
+$oKingHeart = new King();		$oKingHeart->setSuit(new Heart());
+$oKingDiamond = new King();	$oKingDiamond->setSuit(new Diamond());
+$oKingSpade = new King();		$oKingSpade->setSuit(new Spade());
+$oKingClub = new King();		$oKingClub->setSuit(new Club());
+
+
+// Make two players
+$oPlayerOne = new Player();
+$oPlayerTwo = new Player();
+
+// Deck of cards made by objects of cards
+$oDeck = new Deck(
+	$oAceClub, $oAceDiamond, $oAceHeart, $oAceSpade,
+	$oTenClub, $oTenDiamond, $oTenHeart, $oTenSpade,
+	$oNineClub, $oNineDiamond, $oNineHeart, $oNineSpade,
+	$oJackClub, $oJackDiamond, $oJackHeart, $oJackSpade,
+	$oQueenClub, $oQueenDiamond, $oQueenHeart, $oQueenSpade,
+	$oKingClub, $oKingDiamond, $oKingHeart, $oKingSpade
+);
+
+// Shuffle it !
+$oDeck->shuffle();
+
+// Gave 7 cards to both players
+$oDeck->dealCards(7, $oPlayerOne);
+$oDeck->dealCards(7, $oPlayerTwo);
+
+echo "Cards of Player One: \n\n";
+print_r($oPlayerOne->getCards());
+
+echo "\n\n Cards of Player Two: \n\n";
+print_r($oPlayerTwo->getCards());
